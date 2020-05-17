208dae455112553fa6b5c4201c82bd80fbcb9c4e
unknown <youmari300@gmail.com>
2020-05-17 00:39:22 +0000

    Updates

---
+++ precache-manifest.d80e493a04cfd4a76dd29645413eaecc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89123e9cf2a7095a54995505798fd1ab",
+    "url": "/Face-Detection/index.html"
+  },
+  {
+    "revision": "2570630051ec6dd929ef",
+    "url": "/Face-Detection/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "07fd328f94b2dc21ccdc",
+    "url": "/Face-Detection/static/css/main.07842c00.chunk.css"
+  },
+  {
+    "revision": "2570630051ec6dd929ef",
+    "url": "/Face-Detection/static/js/2.66d38ada.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/Face-Detection/static/js/2.66d38ada.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07fd328f94b2dc21ccdc",
+    "url": "/Face-Detection/static/js/main.6003b325.chunk.js"
+  },
+  {
+    "revision": "a97464cd36e6ea09ec5f",
+    "url": "/Face-Detection/static/js/runtime-main.f8f830a2.js"
+  },
+  {
+    "revision": "34f0077134147349c1b41c6406bf788e",
+    "url": "/Face-Detection/static/media/brain.34f00771.png"
+  }
+]);
